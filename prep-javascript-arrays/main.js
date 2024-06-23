7bf111fc6963ef7bfd2e57cf1f99c0301eb9db58
Mahdieh Maleki <mahdiehmaleki96@gmail.com>
2024-06-23 17:16:04 +0000

    updated prep-javascript-arrays

--- prep-javascript-arrays/main.js
+++ prep-javascript-arrays/main.js
@@ -6,5 +6,5 @@ const thirdElement = languages[2];
 console.log('the 3rd element of the array is', thirdElement);
 const length = languages.length;
 console.log('the length of the array is', length);
-const lastElement = languages[5];
+const lastElement = languages[length - 1];
 console.log('the last element of the array is', lastElement);
