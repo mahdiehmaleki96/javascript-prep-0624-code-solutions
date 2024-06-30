af3eb4e6a8832b503ac67a353d65f3944137f157
Mahdieh Maleki <mahdiehmaleki96@gmail.com>
2024-06-30 20:34:51 +0000

    completed prep-array-challenge

---
+++ prep-array-challenge/src/tail.js
@@ -0,0 +1,41 @@
+/* exported tail */
+/* global expect, banMethods, tail */
+function tail(array) {
+  const newArray = [];
+  let index = 1;
+  while (index < array.length) {
+    newArray.push(array[index]);
+    index++;
+  }
+  return newArray;
+}
+describe('tail(array)', function () {
+  beforeEach(function () {
+    expect(tail).to.be.a('function');
+    banMethods(tail, 'slice', 'splice', 'pop', 'shift');
+  });
+
+  it('returns the last two values of ["foo", "bar", "baz"]', function () {
+    const input = ['foo', 'bar', 'baz'];
+    const output = tail(input);
+    expect(output).to.deep.equal(['bar', 'baz']);
+  });
+
+  it('returns the last five values of [1, 2, 3, 4, 5, 6]', function () {
+    const input = [1, 2, 3, 4, 5, 6];
+    const output = tail(input);
+    expect(output).to.deep.equal([2, 3, 4, 5, 6]);
+  });
+
+  it('returns the last three values of [true, false, false, true]', function () {
+    const input = [true, false, false, true];
+    const output = tail(input);
+    expect(output).to.deep.equal([false, false, true]);
+  });
+
+  it('returns [] (empty array) for []', function () {
+    const input = [];
+    const output = tail(input);
+    expect(output).to.deep.equal([]);
+  });
+});
